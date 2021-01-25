4745fa47c1463d1521f7aacf226da2aa5ad07374
Tejas-Teju <tejas8196@gmail.com>
2021-01-25 15:01:30 +0530

    Created method "editAnswerByOwner" in AnswerService - Returns answer entity - throws exception if not owner - updates the answer using data layer i.e using AnswerDao

--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -2,6 +2,8 @@ package com.upgrad.quora.service.business;
 
 import com.upgrad.quora.service.dao.AnswerDao;
 import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -16,5 +18,21 @@ public class AnswerService {
     @Transactional(propagation = Propagation.REQUIRED)
     public AnswerEntity createAnswer(AnswerEntity answerEntity) {
         return answerDao.createAnswer(answerEntity);
+    }
+
+    /**
+     * This method passes the edited answer to Dao for merging
+     * @throws AuthorizationFailedException If non-owner tries to edit an answer
+     * returns AnswerEntity
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity editAnswerByOwner(final String editedAnswerContent, final AnswerEntity answerEntity, final UserAuthEntity userAuthEntity) throws AuthorizationFailedException {
+        if (userAuthEntity.getUser() != answerEntity.getUserId()) {
+            throw new AuthorizationFailedException("ATHR-003", "Only the answer owner can edit the answer");
+        }
+
+        answerEntity.setAns(editedAnswerContent);
+        answerDao.updateAnswerContent(answerEntity);
+        return answerEntity;
     }
 }
