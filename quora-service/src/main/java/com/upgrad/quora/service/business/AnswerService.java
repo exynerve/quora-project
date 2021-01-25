42d605a1aafd559b662fb783c24acd0ae08edf89
Tejas-Teju <tejas8196@gmail.com>
2021-01-25 14:50:15 +0530

    Created method createAnswer in service layer - This passes the answer entity to the data layer to persist - Returns the answer entity after persisting in the database

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -0,0 +1,20 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.AnswerDao;
+import com.upgrad.quora.service.entity.AnswerEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AnswerService {
+
+    @Autowired
+    AnswerDao answerDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity createAnswer(AnswerEntity answerEntity) {
+        return answerDao.createAnswer(answerEntity);
+    }
+}
