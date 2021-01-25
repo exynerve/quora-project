4ef3bd6188a72f6420984fb5c06c2871a2ef1fe0
Tejas-Teju <tejas8196@gmail.com>
2021-01-25 15:06:32 +0530

    Created method "getAnswerByUuid" in AnswerDao to return the answer entity - Returns answer entity - Returns null if answer is not found

--- quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -4,6 +4,7 @@ import com.upgrad.quora.service.entity.AnswerEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 @Repository
@@ -19,6 +20,15 @@ public class AnswerDao {
 
     public void updateAnswerContent(AnswerEntity answerEntity) {
         entityManager.merge(answerEntity);
+    }
+
+    // null value is returned if entity/data is not found
+    public AnswerEntity getAnswerByUuid(final String answerId) {
+        try {
+            return entityManager.createNamedQuery("answerByUuid", AnswerEntity.class).setParameter("uuid", answerId).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
     }
 
 }
