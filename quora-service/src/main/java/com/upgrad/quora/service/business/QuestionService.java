e71fd1d58327313828f27090806d3098a7102657
Tejas-Teju <tejas8196@gmail.com>
2021-01-25 13:26:45 +0530

    Created method createQuestion in service layer

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -0,0 +1,20 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity createQuestion(final QuestionEntity questionEntity) {
+        return questionDao.createQuestion(questionEntity);
+    }
+}
