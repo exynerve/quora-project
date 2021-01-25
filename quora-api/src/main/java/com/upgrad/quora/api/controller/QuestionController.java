90b0e5675aa54ffc0c7379522061405a0d502d2d
Tejas-Teju <tejas8196@gmail.com>
2021-01-25 13:37:41 +0530

    Autowired QuestionService and CommonService
    - Retrived the UserAuthEntity using common service
    - Storing all the details from QuestionRequest to QuestionEntity to persist in DB
    - After persisting store the uuid and status in the QuestionResponse and return the ResponseEntity

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -2,6 +2,12 @@ package com.upgrad.quora.api.controller;
 
 import com.upgrad.quora.api.model.QuestionRequest;
 import com.upgrad.quora.api.model.QuestionResponse;
+import com.upgrad.quora.service.business.CommonService;
+import com.upgrad.quora.service.business.QuestionService;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +16,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/question")
 public class QuestionController {
 
+    @Autowired
+    private CommonService commonService;
+
+    @Autowired
+    private QuestionService questionService;
+
+    /**
+     * This method returns all the answer to a question
+     *
+     * @param questionRequest contains the details of question to store in DB
+     * @param accessToken Used for authorization
+     *
+     * @throws AuthorizationFailedException When access token is invalid
+     *
+     * returns question's uuid and message "QUESTION CREATED"
+     */
     @RequestMapping(method = RequestMethod.POST, path = "/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<QuestionResponse> createQuestion(final QuestionRequest questionRequest, @RequestHeader("authorization") final String accessToken) {
-        return new ResponseEntity<QuestionResponse>(HttpStatus.CREATED);
+    public ResponseEntity<QuestionResponse> createQuestion(final QuestionRequest questionRequest, @RequestHeader("authorization") final String accessToken) throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = commonService.authorizeAccessToken(accessToken);
+
+        QuestionEntity questionEntity = new QuestionEntity();
+        questionEntity.setUuid(UUID.randomUUID().toString());
+        questionEntity.setContent(questionRequest.getContent());
+        questionEntity.setDate(LocalDateTime.now());
+        questionEntity.setUserId(userAuthEntity.getUser());
+
+        final QuestionEntity questionEntityCreated = questionService.createQuestion(questionEntity);
+        final QuestionResponse questionResponse = new QuestionResponse().id(questionEntityCreated.getUuid()).status("QUESTION CREATED");
+
+        return new ResponseEntity<QuestionResponse>(questionResponse, HttpStatus.CREATED);
     }
 }
