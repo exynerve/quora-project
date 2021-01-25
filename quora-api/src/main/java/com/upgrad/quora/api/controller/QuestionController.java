b5b83431b327c0adf59cfbc7d2c3953fb132339b
Tejas-Teju <tejas8196@gmail.com>
2021-01-25 13:21:07 +0530

    Created method createQuestion
    - POST method is used to create a Question
    - QuestionRequest and QuestionResponse is used to get the details from the client and send the response details to the client
    - stores it in DB

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -0,0 +1,21 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.QuestionRequest;
+import com.upgrad.quora.api.model.QuestionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    @RequestMapping(method = RequestMethod.POST, path = "/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<QuestionResponse> createQuestion(final QuestionRequest questionRequest, @RequestHeader("authorization") final String accessToken) {
+        return new ResponseEntity<QuestionResponse>(HttpStatus.CREATED);
+    }
+}
