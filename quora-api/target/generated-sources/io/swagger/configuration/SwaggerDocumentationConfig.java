ffc41994268bfe277a55080682f5ca89a7d011dc
Tejas-Teju <tejas8196@gmail.com>
2021-01-25 15:43:09 +0530

    Updated with the latest maven build

--- quora-api/target/generated-sources/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ quora-api/target/generated-sources/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -10,7 +10,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-01-25T14:20:13.787+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-01-25T15:41:59.970+05:30")
 
 @Configuration
 public class SwaggerDocumentationConfig {
